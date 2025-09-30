0c388cf25619883543982e4c80a6e0bdda98e0ac
Florian Beese <f.beese@tu-braunschweig.de>
2025-09-30 15:44:24 +0200

    test(BinaryPredicate): updated tests to check if returned optional of evaluation has value

--- src/test/java/de/featjar/formula/structure/predicate/BinaryPredicateTest.java
+++ src/test/java/de/featjar/formula/structure/predicate/BinaryPredicateTest.java
@@ -12,9 +12,13 @@ public class BinaryPredicateTest {
 	@Test
 	void testLessEqual() {
 		LessEqual lessEqual = new LessEqual();
-		
+		assertTrue(lessEqual.evaluate(Arrays.asList(7,100)).isPresent());
 		assertTrue(lessEqual.evaluate(Arrays.asList(7,100)).get());
+		
+		assertTrue(lessEqual.evaluate(Arrays.asList(123,100)).isPresent());
 		assertFalse(lessEqual.evaluate(Arrays.asList(123, 100)).get());
+		
+		assertTrue(lessEqual.evaluate(Arrays.asList(123, 123)).isPresent());
 		assertTrue(lessEqual.evaluate(Arrays.asList(123, 123)).get());
 		
 		assertFalse(lessEqual.evaluate(Arrays.asList(123, null)).isPresent());
@@ -26,8 +30,13 @@ public class BinaryPredicateTest {
 	void testEquals() {
 		Equals equals = new Equals();
 		
+		assertTrue(equals.evaluate(Arrays.asList(7, 100)).isPresent());
 		assertFalse(equals.evaluate(Arrays.asList(7,100)).get());
+		
+		assertTrue(equals.evaluate(Arrays.asList(123, 100)).isPresent());
 		assertFalse(equals.evaluate(Arrays.asList(123, 100)).get());
+		
+		assertTrue(equals.evaluate(Arrays.asList(123, 123)).isPresent());
 		assertTrue(equals.evaluate(Arrays.asList(123, 123)).get());
 		
 		assertFalse(equals.evaluate(Arrays.asList(123, null)).isPresent());
@@ -39,8 +48,13 @@ public class BinaryPredicateTest {
 	void testLess() {
 		LessThan lessThan = new LessThan();
 		
+		assertTrue(lessThan.evaluate(Arrays.asList(7, 100)).isPresent());
 		assertTrue(lessThan.evaluate(Arrays.asList(7,100)).get());
+		
+		assertTrue(lessThan.evaluate(Arrays.asList(123, 100)).isPresent());
 		assertFalse(lessThan.evaluate(Arrays.asList(123, 100)).get());
+		
+		assertTrue(lessThan.evaluate(Arrays.asList(123, 123)).isPresent());
 		assertFalse(lessThan.evaluate(Arrays.asList(123, 123)).get());
 		
 		assertFalse(lessThan.evaluate(Arrays.asList(123, null)).isPresent());
@@ -52,8 +66,13 @@ public class BinaryPredicateTest {
 	void testGreaterEqual() {
 		GreaterEqual greaterEqual = new GreaterEqual();
 		
+		assertTrue(greaterEqual.evaluate(Arrays.asList(7, 100)).isPresent());
 		assertFalse(greaterEqual.evaluate(Arrays.asList(7,100)).get());
+		
+		assertTrue(greaterEqual.evaluate(Arrays.asList(123, 100)).isPresent());
 		assertTrue(greaterEqual.evaluate(Arrays.asList(123, 100)).get());
+		
+		assertTrue(greaterEqual.evaluate(Arrays.asList(123, 123)).isPresent());
 		assertTrue(greaterEqual.evaluate(Arrays.asList(123, 123)).get());
 		
 		assertFalse(greaterEqual.evaluate(Arrays.asList(123, null)).isPresent());
@@ -65,20 +84,32 @@ public class BinaryPredicateTest {
 	void testNotEquals() {
 		NotEquals notEquals = new NotEquals();
 		
+		assertTrue(notEquals.evaluate(Arrays.asList(7, 100)).isPresent());
 		assertTrue(notEquals.evaluate(Arrays.asList(7,100)).get());
+		
+		assertTrue(notEquals.evaluate(Arrays.asList(123, 100)).isPresent());
 		assertTrue(notEquals.evaluate(Arrays.asList(123, 100)).get());
+		
+		assertTrue(notEquals.evaluate(Arrays.asList(123, 123)).isPresent());
 		assertFalse(notEquals.evaluate(Arrays.asList(123, 123)).get());
+		
 		
 		assertFalse(notEquals.evaluate(Arrays.asList(123, null)).isPresent());
 		assertFalse(notEquals.evaluate(Arrays.asList(null,123)).isPresent());
 		assertFalse(notEquals.evaluate(Arrays.asList(null, null)).isPresent());
 	}
 	
+	@Test
 	void testGreaterThan() {
 		GreaterThan greaterThan = new GreaterThan();
 		
+		assertTrue(greaterThan.evaluate(Arrays.asList(7, 100)).isPresent());
 		assertFalse(greaterThan.evaluate(Arrays.asList(7,100)).get());
+		
+		assertTrue(greaterThan.evaluate(Arrays.asList(123, 100)).isPresent());
 		assertTrue(greaterThan.evaluate(Arrays.asList(123, 100)).get());
+		
+		assertTrue(greaterThan.evaluate(Arrays.asList(123, 123)).isPresent());
 		assertFalse(greaterThan.evaluate(Arrays.asList(123, 123)).get());
 		
 		assertFalse(greaterThan.evaluate(Arrays.asList(123, null)).isPresent());
